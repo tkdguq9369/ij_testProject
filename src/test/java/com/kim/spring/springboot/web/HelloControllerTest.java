3a62185586505362aa7431c4b5260f594ad6e6a0
kim <tkdguq9369@naver.com>
2021-04-14 22:25:34 +0900

    lombok test clear

---
+++ src/test/java/com/kim/spring/springboot/web/HelloControllerTest.java
@@ -0,0 +1,27 @@
+package com.kim.spring.springboot.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void hello가_리턴된다() throws Exception {
+        String hello = "hello";
+        mvc.perform(get("/hello")) // "/hello"주소로 http get요청
+                .andExpect(status().isOk())  // perform의 결과 (ex) 200,300,400,500...) 검증 -> ok = 200인지 아닌지 검증
+                .andExpect(content().string(hello)); // 응답 본문의 내용을 검증 ("hello"를 리턴하는지 검증)
+    }
+}
