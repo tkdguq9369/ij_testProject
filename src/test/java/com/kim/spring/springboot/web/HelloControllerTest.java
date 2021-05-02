a4014786b0fbe77beef4a7dccfb8a8c6d5c6f56c
kim <tkdguq9369@naver.com>
2021-05-02 21:11:18 +0900

    ~ helloDto return test

--- src/test/java/com/kim/spring/springboot/web/HelloControllerTest.java
+++ src/test/java/com/kim/spring/springboot/web/HelloControllerTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -23,5 +24,18 @@ public class HelloControllerTest {
         mvc.perform(get("/hello")) // "/hello"주소로 http get요청
                 .andExpect(status().isOk())  // perform의 결과 (ex) 200,300,400,500...) 검증 -> ok = 200인지 아닌지 검증
                 .andExpect(content().string(hello)); // 응답 본문의 내용을 검증 ("hello"를 리턴하는지 검증)
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto")
+                .param("name", name)
+                .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
     }
 }
