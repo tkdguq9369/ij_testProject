0a0fa71bbcbafcffc271d7c4597e6e616a06e0c0
kim <tkdguq9369@naver.com>
2021-06-15 20:56:45 +0900

    스프링 시큐리티 / 9.5 codeDeploy 자동 배포 테스트 ~365p

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,11 +7,11 @@ echo "> Build 파일 복사"
 
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
-echo "> 현재 구동 중인 애플리케이션 pid 확인"
+echo "> 현재 구동중인 애플리케이션 pid 확인"
 
 CURRENT_PID=$(pgrep -fl ij_testProject | grep jar | awk '{print $1}')
 
-echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
+echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
     echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
