cf7ed2ffd4c766951c8efbf4af0de6f824f8cdf9
kim <tkdguq9369@naver.com>
2021-06-21 02:27:30 +0900

    카카오로그인 추가를 위해 OAuthAttributes.java 수정

--- src/main/java/com/kim/spring/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/kim/spring/springboot/config/auth/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .headers().frameOptions().disable() //h2-console화면을 사용하기위해 해동 옵션을 disable한다.
                 .and()
                     .authorizeRequests()            //URL별 권한관리를 설정하는 옵션의 시작. authorizeRequests가 선언되어야 antMatchers옵션 사용 가능
-                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()  //관리대상 지정 옵션. URL, HTTP메소드별로 관리 가능. 해당 라인은 전체열람 권한줌.
+                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile", "**/login").permitAll()  //관리대상 지정 옵션. URL, HTTP메소드별로 관리 가능. 해당 라인은 전체열람 권한줌.
                     .antMatchers("/api/v1/**").hasRole(Role.USER.name())                                // "/api/v1/** " 주소를 가진 API는 USER권한을 가진 사람만 가능.
                     .anyRequest().authenticated()   //  anyRequest() 설정값 외 나머지 URL을 나타냄.    authenticated()를 추가하여 나머지 URL은 모두 인증된 사용자들에게만 허용. (로그인 사용자)
                 .and()
